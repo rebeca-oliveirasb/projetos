8113b269a55904ec6c2787f995cd499810897f6b
Rebeca de Oliveira <323003724+rebeca-oliveirasb@users.noreply.github.com>
2026-09-13 17:02:33 -0300

    Projeto em Python para calcular descontos.

---
+++ desconto-loja/README.md
@@ -0,0 +1,9 @@
+# 💳 Projeto - Cálculo de desconto para loja online 🛍️
+
+Este projeto, em Python, foi criado para praticar **estruturas de decisão**, com foco em **if** e **elif**.
+O programa, desenvolvido para uma loja online, realiza cálculo e aplicação de desconto em compras feitas na loja.
+Ele solicita o **valor total da compra**, calcula o desconto que o cliente receberá com **três possibilidades variáveis** de acordo com o valor da compra e exibe: porcentagem e valor do desconto aplicado, e o novo total a ser pago pelo comprador.
+[![Python](https://img.shields.io/badge/Python-3776AB?logo=python&logoColor=white)](https://www.python.org/)
+[![Compras](https://img.shields.io/badge/Compras-🛒-orange)](#)
+[![Desconto](https://img.shields.io/badge/Desconto-💸-green)](#)
+[![Preços](https://img.shields.io/badge/Preços-💰-blue)](#)
---
+++ desconto-loja/app.py
@@ -0,0 +1,21 @@
+# Entrada de dados
+valor_compra = float(input("Digite o valor total da compra: "))
+
+# Processamento e sa[ida de dados
+""" O programa calcula o desconto com base no valor da compra inserido pelo usuário e o aplica a esse valor;
+Na saída, exibe o desconto aplicado e o novo total a ser pago. """
+if valor_compra < 200:
+    desconto = 0.05 * valor_compra
+    print(f"""
+    Desconto de 5% aplicado ao valor da sua compra: R${desconto:.2f}
+    Total a ser pago: R${valor_compra - desconto:.2f}""")
+elif 200 <= valor_compra <300:
+    desconto = 0.10 * valor_compra
+    print(f"""
+    Desconto de 10% aplicado ao valor da sua compra: R${desconto:.2f}
+    Total a ser pago: R${valor_compra - desconto:.2f}""")
+elif valor_compra >= 300:
+    desconto = 0.15 * valor_compra
+    print(f"""
+    Desconto de 15% aplicado ao valor da sua compra: R${desconto:.2f}
+    Total a ser pago: R${valor_compra - desconto:.2f}""")
